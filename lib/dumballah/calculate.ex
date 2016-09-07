759400d1278613930f35bebdb9e23d52066d9f68
Zura Guerra <guerrazuramx@gmail.com>
2016-09-07 01:03:17 -0500

    Calculate if a give date timestamp has already passed

--- lib/dumballah/calculate.ex
+++ lib/dumballah/calculate.ex
@@ -3,7 +3,9 @@ defmodule Dumballah.Calculate do
 
   def add_time(current, unit, multiplier),
     do: current + Convert.to_seconds(unit, multiplier)
-    
+
   def substract_time(current, unit, multiplier),
     do: current - Convert.to_seconds(unit, multiplier)
+
+  def has_passed?(timestamp), do: Dumballah.now >= timestamp
 end
