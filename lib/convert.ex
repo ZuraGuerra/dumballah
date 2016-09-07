a5bb79e7160c05150d585738964594cff2b50ff3
Zura Guerra <guerrazuramx@gmail.com>
2016-09-07 00:37:43 -0500

    Transform time to seconds

---
+++ lib/convert.ex
@@ -0,0 +1,13 @@
+defmodule Dumballah.Convert do
+  # It's dangerous to go alone, take this:
+  # http://erlang.org/doc/man/calendar.html
+
+  @minute_in_seconds :calendar.time_to_seconds({0, 1, 0})
+  @hour_in_seconds :calendar.time_to_seconds({1, 0, 0})
+  @day_in_seconds @hour_in_seconds * 24
+
+  def to_seconds(number), do: to_seconds(:minutes, number)
+  def to_seconds(:minutes, number \\ 1), do: @minute_in_seconds * number
+  def to_seconds(:hours, number \\ 1), do: @hour_in_seconds * number
+  def to_seconds(:days, number \\ 1), do: @day_in_seconds * number
+end
