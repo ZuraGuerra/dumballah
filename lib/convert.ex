a1a6960b9d526b68a89b9a489bc7e6a5ce8f2879
Zura Guerra <guerrazuramx@gmail.com>
2016-09-07 00:46:13 -0500

    Convert dates from and to unix

--- lib/convert.ex
+++ lib/convert.ex
@@ -6,6 +6,9 @@ defmodule Dumballah.Convert do
   @hour_in_seconds :calendar.time_to_seconds({1, 0, 0})
   @day_in_seconds @hour_in_seconds * 24
 
+  defdelegate to_unix(date), to: DateTime, as: :to_unix
+  defdelegate from_unix(date), to: DateTime, as: :from_unix!
+
   def to_seconds(number), do: to_seconds(:minutes, number)
   def to_seconds(:minutes, number \\ 1), do: @minute_in_seconds * number
   def to_seconds(:hours, number \\ 1), do: @hour_in_seconds * number
