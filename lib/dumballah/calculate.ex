f30223541856d10133288679578b87b81cb126e2
Zura Guerra <guerrazuramx@gmail.com>
2016-09-07 15:38:55 -0500

    Calculate dawn of erlang date in unix time

--- lib/dumballah/calculate.ex
+++ lib/dumballah/calculate.ex
@@ -8,4 +8,11 @@ defmodule Dumballah.Calculate do
     do: current - Convert.to_seconds(unit, multiplier)
 
   def has_passed?(timestamp), do: Dumballah.now >= timestamp
+
+  def date_from_dawn({year, month, day}) do
+    %DateTime{calendar: Calendar.ISO, day: day, hour: 0, microsecond: {0, 0},
+              minute: 0, month: month, second: 0, std_offset: 0,
+              time_zone: "Etc/UTC", utc_offset: 0, year: year, zone_abbr: "UTC"}
+    |> DateTime.to_unix
+  end
 end
