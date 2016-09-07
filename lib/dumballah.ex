4700ab73be986776757ac89bb43c40493a9fc343
Zura Guerra <guerrazuramx@gmail.com>
2016-09-07 01:05:34 -0500

    Return unix epoch

--- lib/dumballah.ex
+++ lib/dumballah.ex
@@ -3,4 +3,7 @@ defmodule Dumballah do
     {megaseconds, seconds, _} = :os.system_time
     (megaseconds * 1000000) + seconds
   end
+
+  def unix_epoch,
+    do: :calendar.datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}})
 end
