9682d1f8283bc20cf889413039c73fe6de9e5a0f
Zura Guerra <guerrazuramx@gmail.com>
2016-09-20 10:09:33 -0500

    Format Construct module

--- lib/dumballah/construct.ex
+++ lib/dumballah/construct.ex
@@ -9,8 +9,17 @@ defmodule Dumballah.Construct do
   def elixir_datetime(year, month, day,
                       hour \\ 0, minute \\ 0, second \\ 0,
                       microsecond \\ {0,0}) do
-    %DateTime{calendar: Calendar.ISO, day: day, hour: hour, microsecond: microsecond,
-              minute: minute, month: month, second: second, std_offset: 0,
-              time_zone: "Etc/UTC", utc_offset: 0, year: year, zone_abbr: "UTC"}
+    %DateTime{calendar: Calendar.ISO,
+              year: year,
+              month: month,
+              day: day,
+              hour: hour,
+              minute: minute,
+              second: second,
+              microsecond: microsecond,
+              std_offset: 0,
+              time_zone: "Etc/UTC",
+              utc_offset: 0,
+              zone_abbr: "UTC"}
   end
 end
