00af779122f860c598b9036ece05a7bdd3d7bc1f
Zura Guerra <guerrazuramx@gmail.com>
2016-09-20 10:03:20 -0500

    Test Convert and Calculate modules from docs

---
+++ test/dumballah/calculate_test.exs
@@ -0,0 +1,4 @@
+defmodule Dumballah.CalculateTest do
+  use ExUnit.Case, async: true
+  doctest Dumballah.Calculate
+end
---
+++ test/dumballah/convert_test.exs
@@ -0,0 +1,4 @@
+defmodule Dumballah.ConvertTest do
+  use ExUnit.Case, async: true
+  doctest Dumballah.Convert
+end
